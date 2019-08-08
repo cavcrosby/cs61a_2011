e17eb13c2f55b296e462fae036ec30e07e7d79a3
Conner Crosby <crosbyco3@gmail.com>
2019-08-08 07:33:46 -0500

    rearranged some code in e1-2-9.scm

--- book_exercises/E1-2-9.scm
+++ book_exercises/E1-2-9.scm
@@ -1,13 +1,15 @@
 (define (simps_rule f a b n)
 	(define delta_x (/ (- b a) n))
-	(define (compute f a b n delta_x)
-			(if (equal? delta_x b)
-				0
-				(* (/ (/ (- b a) n) 3)
-				(cond ((= delta_x b) (+ (f delta_x) (compute f a b n (+ delta_x (/ (- b a) n)))))
-					  ((= delta_x a) (+ (f delta_x) (compute f a b n (+ delta_x (/ (- b a) n)))))
-					  (else (if (remainder (/ (/ (- b a) n) delta_x) 0)
-								(+ (* (f delta_x) 2) (compute f a b n (+ delta_x (/ (- b a) n))))
-								(+ (* (f delta_x) 4) (compute f a b n (+ delta_x (/ (- b a) n))))))))))
+	; take (* (/ (/ (- b a) n) 3) out so it is only computed once, while it is multipled by numberous numbers added
 	(compute f a b n delta_x)
 )
+
+(define (compute f a b n delta_x)
+			(* (/ (/ (- b a) n) 3)
+			(if (equal? delta_x b)
+				1
+				(cond ((= delta_x b) (+ (f delta_x) (compute f a b n (+ delta_x (/ (- b a) n)))))
+					  ((= delta_x a) (+ (f delta_x) (compute f a b n (+ delta_x (/ (- b a) n)))))
+					  (else (if (/ (/ (/ (- b a) n) delta_x) 1)
+								(+ (* (f delta_x) 2) (compute f a b n (+ delta_x (/ (- b a) n))))
+								(+ (* (f delta_x) 4) (compute f a b n (+ delta_x (/ (- b a) n))))))))))
