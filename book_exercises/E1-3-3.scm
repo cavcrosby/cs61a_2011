a26b854c92c0d78fae17372a5044c8ad8fbcae0c
Conner Crosby <crosbyco3@gmail.com>
2019-08-14 22:16:34 -0400

    changed prime func to be easier to read

--- book_exercises/E1-3-3.scm
+++ book_exercises/E1-3-3.scm
@@ -2,17 +2,17 @@
 	(cond ((> a b) null-value)
 		  ((predicate a b) (combiner (term a) (filtered-accumulate combiner predicate null-value term (next a) next b)))
 		  (else (filtered-accumulate combiner predicate null-value term (next a) next b))))
-		  
+
+
 (define (prime? x n)
-	(define i 2) ; 0 and 1 are not primes, 2 and 3 are
-	(define (numbers_btwn_i_x i) (if (>= i x) '() (se i (numbers_btwn_i_x (+ 1 i)))))
-	(define (is_it_prime? numbers_btwn_i_x)
-		(cond ((or (empty? numbers_btwn_i_x) (= (first numbers_btwn_i_x) x)) #t)
-			  ((= (remainder x (first numbers_btwn_i_x)) 0) #f)
-			  (else (is_it_prime? (bf numbers_btwn_i_x)))))
-			
-	(is_it_prime? (numbers_btwn_i_x i)))
-	; any number below 2 will return #t, this procedure can be improved
+	(define starting_prime 2)
+	(define (prime_helper y)
+		(cond ((= x y) #t)
+			  ((= (modulo x y) 0) #f)
+			  (else (prime_helper (+ y 1))))) 
+	(cond ((< x starting_prime) #f) ; looking at positive primes
+		  (else (prime_helper starting_prime))))	
+				  
 				  
 (define (relative-prime? i n)
 	(cond ((<= i 0) #f) ; we are just looking for positive integers GCD(i, n) = 1
