8b701e9ef20e973baf3ecf3040896e02dad7d654
Conner Crosby <crosbyco3@gmail.com>
2019-08-20 00:03:53 -0400

    added new strategy and added card types of j q k

--- cs61a_projects/twenty-one.scm
+++ cs61a_projects/twenty-one.scm
@@ -1,39 +1,44 @@
 (define (twenty-one strategy)
   (define (play-dealer customer-hand dealer-hand-so-far rest-of-deck)
-    (cond ((> (best-total dealer-hand-so-far) 21) 1)
+    (cond ((> (best-total dealer-hand-so-far) 21) 1) ; customer wins if dealer busts
 	  ((< (best-total dealer-hand-so-far) 17)
 	   (play-dealer customer-hand
 			(se dealer-hand-so-far (first rest-of-deck))
 			(bf rest-of-deck)))
-	  ((< (best-total customer-hand) (best-total dealer-hand-so-far)) -1)
-	  ((= (best-total customer-hand) (best-total dealer-hand-so-far)) 0)
-	  (else 1)))
+	  ((< (best-total customer-hand) (best-total dealer-hand-so-far)) -1) ; dealer wins
+	  ((= (best-total customer-hand) (best-total dealer-hand-so-far)) 0) ; tie
+	  (else 1))) ; customer wins
 
   (define (play-customer customer-hand-so-far dealer-up-card rest-of-deck)
     (cond ((> (best-total customer-hand-so-far) 21) -1)
-	  ((strategy customer-hand-so-far dealer-up-card)
-	   (play-customer (se customer-hand-so-far (first rest-of-deck))
-			  dealer-up-card
-			  (bf rest-of-deck)))
+		  ((strategy customer-hand-so-far dealer-up-card)
+	       (play-customer (se customer-hand-so-far (first rest-of-deck)) dealer-up-card (bf rest-of-deck)))
 	  (else
 	   (play-dealer customer-hand-so-far
 			(se dealer-up-card (first rest-of-deck))
 			(bf rest-of-deck)))))
 			
-    (define (best-total hand-of-cards)
+
+  (let ((deck (make-deck)))
+    (play-customer (se (first deck) (first (bf deck)))
+		   (first (bf (bf deck)))
+		   (bf (bf (bf deck))))))
+
+(define (play-n strategy n)
+		(if (= n 0) 0 (+ (play-n strategy (- n 1))(twenty-one strategy))))
+		
+(define stop-at-17 (lambda (customer-hand-so-far dealer-up-card) (if (< (best-total customer-hand-so-far) 17) #t #f)))
+
+(define (best-total hand-of-cards)
 		(define (best-total-running hand-of-cards running-total)
 			(cond ((empty? hand-of-cards) running-total)
 				  ((equal? (bl (first hand-of-cards)) 'a) 
 					(let ((new-running-total (+ 11 (best-total-running (bf hand-of-cards) running-total)))) 
 						(if (< 21 new-running-total) (- new-running-total 10) new-running-total)))
+				  ((member? (bl (first hand-of-cards)) '(j q k)) (best-total-running (bf hand-of-cards) (+ running-total 10)))
 				  (else (best-total-running (bf hand-of-cards) (+ running-total (bl (first hand-of-cards)))))))
 		(best-total-running hand-of-cards 0))
-
-  (let ((deck (make-deck)))
-    (play-customer (se (first deck) (first (bf deck)))
-		   (first (bf (bf deck)))
-		   (bf (bf (bf deck))))) )
-
+		
 (define (make-ordered-deck)
   (define (make-suit s)
     (every (lambda (rank) (word rank s)) '(A 2 3 4 5 6 7 8 9 10 J Q K)) )
@@ -49,5 +54,3 @@
 	deck
     	(move-card deck '() (random size)) ))
   (shuffle (make-ordered-deck) 52) )
-
-
