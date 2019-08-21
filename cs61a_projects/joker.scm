5a5276a885ef45c2b26ed45d8a83c6bedc70ac4f
Conner Crosby <crosbyco3@gmail.com>
2019-08-21 11:29:56 -0400

    joker added, changes made to include it into the deck and to calculate a joker btwn 1 and 11

---
+++ cs61a_projects/joker.scm
@@ -0,0 +1,85 @@
+(define (twenty-one strategy)
+  (define (play-dealer customer-hand dealer-hand-so-far rest-of-deck)
+    (cond ((> (best-total dealer-hand-so-far) 21) 1) ; customer wins if dealer busts
+	  ((< (best-total dealer-hand-so-far) 17)
+	   (play-dealer customer-hand
+			(se dealer-hand-so-far (first rest-of-deck))
+			(bf rest-of-deck)))
+	  ((< (best-total customer-hand) (best-total dealer-hand-so-far)) -1) ; dealer wins
+	  ((= (best-total customer-hand) (best-total dealer-hand-so-far)) 0) ; tie
+	  (else 1))) ; customer wins
+
+  (define (play-customer customer-hand-so-far dealer-up-card rest-of-deck)
+    (cond ((> (best-total customer-hand-so-far) 21) -1)
+		  ((strategy customer-hand-so-far dealer-up-card)
+	       (play-customer (se customer-hand-so-far (first rest-of-deck)) dealer-up-card (bf rest-of-deck)))
+	  (else
+	   (play-dealer customer-hand-so-far
+			(se dealer-up-card (first rest-of-deck))
+			(bf rest-of-deck)))))
+			
+
+  (let ((deck (make-deck)))
+    (play-customer (se (first deck) (first (bf deck)))
+		   (first (bf (bf deck)))
+		   (bf (bf (bf deck))))))
+
+(define (play-n strategy n)
+		(if (= n 0) 0 (+ (play-n strategy (- n 1))(twenty-one strategy))))
+		
+(define stop-at-17 (lambda (customer-hand-so-far dealer-up-card) (if (< (best-total customer-hand-so-far) 17) #t #f)))
+
+(define stop-at (lambda (n) : (lambda (customer-hand-so-far dealer-up-card) (if (< (best-total customer-hand-so-far) n) #t #f))))
+
+(define stop-at
+
+(define dealer-sensitive (lambda (customer-hand-so-far dealer-up-card) (if (or (and (member? (bl dealer-up-card) '(a 7 8 9 10 j q k)) ((stop-at 17) customer-hand-so-far dealer-up-card)) 
+																		       (and (member? (bl dealer-up-card) '(2 3 4 5 6)) ((stop-at 12) customer-hand-so-far dealer-sensitive))) #t #f)))
+																			  
+(define valentine (lambda (customer-hand-so-far dealer-up-card) (if (member? 'h (every (lambda (x) (bf x)) customer-hand-so-far)) 
+																		 ((stop-at 19) customer-hand-so-far dealer-up-card) 
+																		 ((stop-at 17) customer-hand-so-far dealer-up-card))))
+																		
+																		
+																		
+(define (valentine2 customer-hand-so-far dealer-up-card) (if (member? 'h (every (lambda (x) (bf x)) customer-hand-so-far)) 
+																		 ((stop-at 19) customer-hand-so-far dealer-up-card) 
+																		 ((stop-at 17) customer-hand-so-far dealer-up-card)))
+																		 
+(define suit-strategy (lambda (suit strategy_to_use_dnt strategy_to_use_d) (lambda (customer-hand-so-far dealer-up-card) (if (member? suit (every (lambda (x) (bf x)) customer-hand-so-far)) 
+																		(strategy_to_use_d customer-hand-so-far dealer-up-card)
+																		(strategy_to_use_dnt customer-hand-so-far dealer-up-card)))))
+																		
+(define majority (lambda (strat1 strat2 strat3) (lambda (customer-hand-so-far dealer-up-card) 
+	(if 
+		(<= 2 (+ (if (equal? #t (strat1 customer-hand-so-far dealer-up-card)) 1 0) (if (equal? #t (strat2 customer-hand-so-far dealer-up-card)) 1 0) (if (equal? #t (strat3 customer-hand-so-far dealer-up-card)) 1 0))) #t #f))))
+
+
+(define reckless (lambda (strat1) (lambda (customer-hand-so-far dealer-up-card) (if (and (strat1 customer-hand-so-far dealer-up-card) (< 21 (best-total customer-hand-so-far))) #t #f)))) 
+
+(define (best-total hand-of-cards)
+		(define (best-total-running hand-of-cards running-total)
+			(cond ((empty? hand-of-cards) running-total)
+				  ((equal? (bl (first hand-of-cards)) 'a) 
+					(let ((new-running-total (+ 11 (best-total-running (bf hand-of-cards) running-total)))) 
+						(if (< 21 new-running-total) (- new-running-total 10) new-running-total)))
+				  ((equal? (first hand-of-cards) 'o) (best-total-running (bf hand-of-cards) (+ running-total (let((number (random 11))) (if (= number 0) 1 number)))))
+				  ((member? (bl (first hand-of-cards)) '(j q k)) (best-total-running (bf hand-of-cards) (+ running-total 10)))
+				  (else (best-total-running (bf hand-of-cards) (+ running-total (bl (first hand-of-cards)))))))
+		(best-total-running hand-of-cards 0))
+		
+(define (make-ordered-deck)
+  (define (make-suit s)
+    (every (lambda (rank) (word rank s)) '(A 2 3 4 5 6 7 8 9 10 J Q K)) ) ; takes each rank type and appends a suit to each rank (e.g. ah 2h 3h 4h...)
+  (se (make-suit 'H) (make-suit 'S) (make-suit 'D) (make-suit 'C)) )
+
+(define (make-deck)
+  (define (shuffle deck size)
+    (define (move-card in out which)
+      (if (= which 0)
+	  (se (first in) (shuffle (se (bf in) out) (- size 1)))
+	  (move-card (bf in) (se (first in) out) (- which 1)) ))
+    (if (= size 0)
+		deck
+		(move-card deck '() (random size)) ))
+  (shuffle (se (make-ordered-deck) '(o o)) 54) ) ; o is denoted as joker
