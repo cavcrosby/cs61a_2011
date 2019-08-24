fc06ca06e55bc1c2e5c542fde93e6c85d7372d66
Conner Crosby <crosbyco3@gmail.com>
2019-08-24 14:46:34 -0400

    cleanup for E1-3-8

--- book_exercises/E1-3-8.scm
+++ book_exercises/E1-3-8.scm
@@ -3,5 +3,3 @@
 	(define (euler-helper n k t)
 		(if (= k 0) (/ 1 t) (let ((d (cal_d (- k 1)))) (euler-helper n (- k 1) (+ d (/ n t))))))
 	(let ((d (cal_d (- k 1)))) (euler-helper n (- k 1) (/ n d))))
-
-;(if (equal? (modulo (- k 1) 3) 1)
