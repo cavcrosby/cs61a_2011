e827eb4bac2951e5cd1f0fd68f61ba2837310085
Conner Crosby <crosbyco3@gmail.com>
2019-08-29 14:18:00 -0400

    reorganized procedures in making rectangle

--- book_exercises/E2-3.scm
+++ book_exercises/E2-3.scm
@@ -20,65 +20,48 @@
 	(display ",")
 	(display (y-point p))
 	(display ")"))
-	
-	
-
-
-; testing code
-(define seg1 (make-segment (make-point 2 5) (make-point 8 5)))
-(define seg2 (make-segment (make-point 2 3) (make-point 8 3)))
-
-(define seg3 (make-segment (make-point 3 (- 5)) (make-point 3 4)))
-(define seg4 (make-segment (make-point 5 (- 5)) (make-point 5 4)))
-
-(define hr (make-rectangle seg1 seg2))
-(define vr (make-rectangle seg3 seg4))
 
 (define (make-rectangle first-segment second-segment)
+	(define (segment-straight? cord segment)
+		(equal? (cord (start-segment segment)) 
+				(cord (end-segment segment))))
+	(define (segments-parallel? cord)
+		(and  (equal? (cord (start-segment first-segment)) 
+						(cord (start-segment second-segment)))
+			  (equal? (cord (end-segment first-segment)) 
+						(cord (end-segment second-segment)))))
 	(define (hor-parallel? first-segment second-segment)
 		(if 
 			(and 
-				(and (equal? (y-point (start-segment first-segment)) 
-						 (y-point (end-segment first-segment)))
-					 (equal? (y-point (start-segment second-segment)) 
-						 (y-point (end-segment second-segment))))
-			    (and (equal? (x-point (start-segment first-segment))
-						 (x-point (start-segment second-segment)))
-					 (equal? (x-point (end-segment first-segment))
-						 (x-point (end-segment second-segment)))))
+				(and (segment-straight? y-point first-segment) (segment-straight? y-point second-segment))
+			    (and (segments-parallel? x-point)))
 			#t
 			#f))
 	(define (ver-parallel? first-segment second-segment)
 		(if 
 			(and 
-				(and  (equal? (x-point (start-segment first-segment)) 
-						 (x-point (end-segment first-segment)))
-					  (equal? (x-point (start-segment second-segment)) 
-						 (x-point (end-segment second-segment))))
-				 (and (equal? (y-point (start-segment first-segment))
-						 (y-point (start-segment second-segment)))
-					  (equal? (y-point (end-segment first-segment))
-						 (y-point (end-segment second-segment)))))
+				(and (segment-straight? x-point first-segment) (segment-straight? x-point second-segment))
+			    (and (segments-parallel? y-point)))
 			#t
 			#f))
 	(let ((is-hor-parallel (hor-parallel? first-segment second-segment)) (is-ver-parallel (ver-parallel? first-segment second-segment)))
-		(cond ((not (or is-hor-parallel is-ver-parallel)) (error "Need parallel segments (starting/ending points must align either by either x or y)..."))
-			  (is-hor-parallel (if (> (y-point (start-segment first-segment)) (y-point (start-segment second-segment))) 
-									 (list (start-segment second-segment) (start-segment first-segment) (end-segment second-segment) (end-segment first-segment))
-									 (list (start-segment first-segment) (start-segment second-segment) (end-segment first-segment) (end-segment second-segment))))
-			  (else (if (> (x-point (start-segment first-segment)) (x-point (start-segment second-segment))) 
-									 (list (start-segment second-segment) (start-segment first-segment) (end-segment second-segment) (end-segment first-segment)) 
-									 (list (start-segment first-segment) (start-segment second-segment) (end-segment first-segment) (end-segment second-segment)))))))
+		(cond ((not (or is-hor-parallel is-ver-parallel)) 
+						(error "Need parallel segments (starting/ending points must align either by either x or y)..."))
+			  (is-hor-parallel 
+					(if (> (y-point (start-segment first-segment)) (y-point (start-segment second-segment))) 
+						(list (start-segment second-segment) (start-segment first-segment) (end-segment second-segment) (end-segment first-segment))
+						(list (start-segment first-segment) (start-segment second-segment) (end-segment first-segment) (end-segment second-segment))))
+			  (else 
+					(if (> (x-point (start-segment first-segment)) (x-point (start-segment second-segment))) 
+						(list (start-segment second-segment) (start-segment first-segment) (end-segment second-segment) (end-segment first-segment)) 
+						(list (start-segment first-segment) (start-segment second-segment) (end-segment first-segment) (end-segment second-segment)))))))
 									 
 									 
-(define (calculation name)
-	(lambda (rectangle) (name rectangle)))
+(define (width rectangle across-cord)
+	(abs (- (across-cord (get-coordinate rectangle)) (across-cord (get-coordinate (other-coordinates rectangle))))))
 	
-(define (width rectangle cord-point)
-	(abs (- (cord-point (get-coordinate rectangle)) (cord-point (get-coordinate (other-coordinates rectangle))))))
-	
-(define (length rectangle cord-point)
-	(abs (- (cord-point (get-coordinate rectangle)) (cord-point (get-coordinate (other-coordinates (other-coordinates rectangle)))))))
+(define (length rectangle across-cord)
+	(abs (- (across-cord (get-coordinate rectangle)) (across-cord (get-next-coordinate (other-coordinates rectangle))))))
 	
 (define (perimeter rectangle)
 	(if (equal? (y-point (get-coordinate rectangle)) (y-point (get-coordinate (other-coordinates rectangle)))) ; if we have vertical parallel rectangle
@@ -91,4 +74,15 @@
 		(* (width rectangle y-point) (length rectangle x-point))))
 
 (define get-coordinate first)
+(define get-next-coordinate (lambda (x) (get-coordinate (other-coordinates x))))
 (define other-coordinates butfirst)
+
+; testing code
+(define hr (make-rectangle seg1 seg2))
+(define vr (make-rectangle seg3 seg4))
+
+(define seg1 (make-segment (make-point 2 5) (make-point 8 5)))
+(define seg2 (make-segment (make-point 2 3) (make-point 8 3)))
+
+(define seg3 (make-segment (make-point 3 (- 5)) (make-point 3 4)))
+(define seg4 (make-segment (make-point 5 (- 5)) (make-point 5 4)))
