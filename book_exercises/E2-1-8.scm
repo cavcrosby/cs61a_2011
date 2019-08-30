3de364a97495f16b9a3b55eee2ffe5eed400f7db
Conner Crosby <crosbyco3@gmail.com>
2019-08-30 14:07:14 -0400

    completed reverse function

--- book_exercises/E2-1-8.scm
+++ book_exercises/E2-1-8.scm
@@ -1,17 +1,18 @@
-(define (reverse list_fp)
-	(cond ((empty? (current-pair-next-points-to list_fp)) (adjoin (car list_fp)))
-		  ;((empty? (next-to-last-pair list_fp)) (car list_fp))
-		  (else (((adjoin-combined-pairs 
-				((lambda (g) (lambda (p) (lambda (next-next-pair) (g (cons p next-next-pair)))))(current-pair-value-points-to list_fp)) (reverse (current-pair-next-points-to list_fp)))
-				)))))
-
+(define (reverse list-fp)
+	((reverse-helper list-fp) ()))
 	
+(define (reverse-helper list-fp)
+	(cond ((empty? (current-pair-next-points-to list-fp)) (adjoin (car list-fp)))
+		  ;((empty? (next-to-last-pair list-fp)) (car list-fp))
+		  (else ((lambda (g) 
+					((lambda (p) 
+						(lambda (next-next-pair) (g (cons p next-next-pair))))
+					(current-pair-value-points-to list-fp)))
+				(reverse-helper (current-pair-next-points-to list-fp)))
+				)))
+
 (define current-pair-value-points-to car)
 (define current-pair-next-points-to cdr)
 
 (define (adjoin first_pair) (lambda (next_pair) (cons first_pair next_pair)))
-;((lambda (first_pair) : (lambda (next_pair) (cons first_pair next_pair))) (car list_fp))
-
-(define (adjoin-combined-pairs combined-pairs) (lambda (next-next-pair) ((combined-pairs next-next-pair))))
-;((lambda (combined_pairs): (lambda (next_pair) (cons combined_pairs next_pair))) 
-;					((reverse (current-pair-next-points-to list_fp)) (current-pair-value-points-to list_fp)))
+;((lambda (first_pair) : (lambda (next_pair) (cons first_pair next_pair))) (car list-fp))
