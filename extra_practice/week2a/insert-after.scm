0174100245ee6e805638d05054fa62d0964cf952
Conner Crosby <crosbyco3@gmail.com>
2019-09-02 14:44:50 -0400

    put helper func for insert-after inside insert-after

--- extra_practice/week2a/insert-after.scm
+++ extra_practice/week2a/insert-after.scm
@@ -1,8 +1,7 @@
 (define (insert-after item mark ls)
-	(insert-after-helper item mark 0 (cdr ls) (list (car ls))))
-	
-(define (insert-after-helper item mark counter ls new-ls)
+	(define (insert-after-helper item mark counter ls new-ls)
 		(cond ((empty? ls) (append new-ls (list item))) 
 		((= mark 0) (append (list item) (list (car new-ls)) ls))
 		((>= counter mark) (apply append (append new-ls (list item (car ls))) (list (cdr ls))))
 		(else (insert-after-helper item mark (+ 1 counter) (cdr ls) (append new-ls (list (car ls)))))))
+	(insert-after-helper item mark 0 (cdr ls) (list (car ls))))
