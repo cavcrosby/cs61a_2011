44bd7857a33f0561c1601227c9ce2f74bd3febaa
Conner Crosby <crosbyco3@gmail.com>
2019-10-02 21:04:40 -0400

    not perfect, but should be workable

--- book_exercises/E2-74.scm
+++ book_exercises/E2-74.scm
@@ -28,11 +28,8 @@
 	; predicates
 	
 	(define (coercion? type container-type)
-		(>= (index-of types container-type) (index-of types type)))
+		(<= (index-of types container-type) (index-of types type)))
 	
-	(define (valid-type? type)
-		(if (equal? (index-of types type) -1) #t #f))
-			 
 	(define (of-requested-type? type container-type)
 		(equal? type container-type))
 		
@@ -43,8 +40,6 @@
 	
 	(define get-identifier cadr)
 	
-	(define get-identifier-attr caadr)
-	
 	(define type-tag car)
 	
 	(define rest-of cdr)
@@ -53,71 +48,84 @@
 	
 	; TODO ADD GETTERS FOR SUBTYPES, CONTENTS JUST GIVES ME THE DATA, E.G (cdar (contents org)) gives me the divisions
 	; ADD TRANSFORM FUNC FOR OTHERS TO USE?
-	
-	(define (get-record type-desired container identifier) ; container should be of a specfic data object type, so all divisions, emp-records, or attributes
-		(cond ((not (valid-type? type-desired)) (error "Unknown type of record to get -- get-record:" type))
-			  ((and ; if we don't have the right data and we can perform coercion on the data, try again
-				(not (of-requested-type? 
-						type-desired 
-						(type-tag container)))
-				(coercion? 
-					type-desired 
-					(type-tag container)))
-						(apply append (map (lambda (subtype) (get-record type-desired subtype identifier)) (sub-type-contents container))))
-			  ((not (of-requested-type? type-desired (type-tag container))) '()) ; if we get to the bottom data type with no results, return '()
-			  (else (list (find identifier container)))))
+			   
+			   
+	(define (get-generic type-desired container identifier) ; container should be of a specfic data object type, so all divisions, emp-records, or attributes
+		(cond  ((not (coercion? type-desired (type-tag container))) (error "Cannot perform coercion on container passed in -- " (type-tag container)))
+			   ((not (of-requested-type? type-desired (type-tag container))) (apply append (map (lambda (datum) (get-generic type-desired datum identifier)) (sub-type-contents container))))
+			   (else (find identifier container))))
+			   
 	
 	; utility
 	
 	(define (index-of lst x)
 		(define (index-of-helper lst count)
 			(cond ((null? lst) -1)
-				  ((eq? (car lst) x) count)
-				  (else (+ (index-of-helper (cdr lst) (+ 1 count))))))
+				  ((equal? (car lst) (list x)) count) ; all types are in braces e.g. (organization)
+				  (else (index-of-helper (cdr lst) (+ 1 count)))))
 	  (index-of-helper lst 0))
 	
 	(define (find identifier container-row)
-		(cond ((null? container-row) '())
-			  ((and (equal? (type-tag container-row) 'attr) (equal? identifier (get-identifier-attr container-row))) container-row)
-			  ((and (not (equal? (type-tag container-row) 'attr)) (equal? identifier (get-identifier container-row))) container-row)
-			  (else (find identifier (rest-of container-row)))))
+		(if (equal? identifier (get-identifier container-row))
+			(list container-row)
+			'()))
+			  
+	(trace coercion?)
+	(trace index-of)
 	
 	;; interface to rest of the system
 	(define (tag type e) (attach-tag type e))
 	(put 'make '(organization)
 		(lambda (identifier . divisions) (tag 'organization (make-organization identifier divisions))))
 	(put 'get-data '(organization)
-		(lambda (organization identifier) (get-record 'organization organization identifier)))
+		(lambda (organization identifier) (get-generic 'organization organization identifier)))
 	(put 'make '(division)
 		(lambda (identifier . emp-records) (tag 'division (make-division identifier emp-records))))
 	(put 'get-data '(division)
-		(lambda (division identifier) (get-record 'division division identifier)))
+		(lambda (division identifier) (get-generic 'division division identifier)))
 	(put 'make '(emp-record)
 		(lambda (identifier . emp-attrs) (tag 'emp-record (make-emp-record identifier emp-attrs))))
 	(put 'get-data '(emp-record)
-		(lambda (emp-record identifier) (get-record 'emp-record emp-record identifier)))
+		(lambda (emp-record identifier) (get-generic 'emp-record emp-record identifier)))
 	(put 'make '(attr)
 		(lambda (identifier attr-value) (tag 'attr (make-attr identifier attr-value))))
 	(put 'get-data '(attr)
-		(lambda (identifier value) (get-record 'attr identifier value)))
+		(lambda (identifier value) (get-generic 'attr identifier value)))
 'done)
-
-
-
-
-
 
 ; package install
 (install-division-package)
 
 ; interface procedures
 
-(define (make-organization-data type data identifier)
-	((get 'make (append (list type) '())) data identifier))
+(define (make-organization-data type-desired data identifier)
+	(let ((func (get 'make (append (list type-desired) '()))))
+		(if func
+			(func identifier data)
+			(error "Unknown type-desired -- make-organization-data: " type-desired))))
 
-(define (get-record type data identifier)
-	((get 'get-data (append (list type) '())) data identifier))
+;a
+(define (get-record type-desired data identifier)
+	(let ((func (get 'get-data (append (list type-desired) '()))))
+		(if func
+			(func data identifier)
+			(error "Unknown type of record to get -- get-record: " type-desired))))
+			
+;b			
+(define (get-salary emp div)
+		(get-record 
+			'attr 
+			(car (get-record 'emp-record div emp))
+			'salary))
+;c			
+(define (find-employee-record emp org)
+		(get-record 'emp-record org emp))
 	
+	
+;d
+;The only change to be made, will be that the new company will need to be added as a new division to Insatiable
+
+
 ;;;;;;;;
 
 (define attr1 ((get 'make '(attr)) 'Salary '50000))
@@ -125,6 +133,11 @@
 
 (define emp-record ((get 'make '(emp-record)) 'Conner attr1 attr2))
 
-(define division ((get 'make '(division)) 'Alpha emp-record))
+(define attr3 ((get 'make '(attr)) 'Salary '70000))
+(define attr4 ((get 'make '(attr)) 'Address '(123 Bob Road)))
+
+(define emp2-record ((get 'make '(emp-record)) 'William attr3 attr4))
+
+(define division ((get 'make '(division)) 'Alpha emp-record emp2-record))
 
 (define org ((get 'make '(organization)) 'the-cool-guys division))
