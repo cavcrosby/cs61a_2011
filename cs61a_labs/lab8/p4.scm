d721f2927d6e9dfc9d5d601b3df6c536b73eeb88
Conner Crosby <crosbyco3@gmail.com>
2019-10-23 23:46:36 -0400

    set! does not have its first argument replaced if assuming the substituion model

--- cs61a_labs/lab8/p4.scm
+++ cs61a_labs/lab8/p4.scm
@@ -2,8 +2,8 @@
 	(set! var (+ var 1))
 	var)
 
-; (set! 5 (+ 5 1))
+; (set! var (+ 5 1))
 ;	5)
 ;
-; the actual result will return 5 each time (if 5 is whats passed in), since set! is not a lambda, the var as its first argument will also be replaced by whatever var is passed in as
+; the actual result will return 5 each time (if 5 is whats passed in)
 ;
