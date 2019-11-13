ddf40bb8431fdb58c2c40b353b398b083aee6519
Conner Crosby <crosbyco3@gmail.com>
2019-11-12 23:19:20 -0500

    will finish tomorrow

---
+++ cs61a_homework/week10/p3.scm
@@ -0,0 +1,4 @@
+; #1 could not have been done on the server end because at any point that the user wants to send a message to one or more users, this would only need handling from the client and not the server.
+; #2 could have been entirely done on the client side. As the client side always has a current copy of the clients that are connected to the server. If this is a feature to be included for different kinds of client programs, it might be a better idea to have this as a feature on the server, so its not isolated on a particular client program.
+;
+;
